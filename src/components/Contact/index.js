586be321c9f59bc20f714bb8e5af0b3cec23286a
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-10-08 16:21:32 -0500

    contact form set up to capture form submission

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <section>
             <h1>Contact Me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 {/* name input
                 email input
                 message text area */}
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" onChange={handleChange} defaultValue={formState.name} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" onChange={handleChange} defaultValue={formState.email}/>
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" row="5" />
+                    <textarea name="message" row="5" onChange={handleChange} defaultValue={formState.message} />
                 </div>
                 <button type="sumbit">Submit</button>
             </form>
